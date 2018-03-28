ba8266bd7ef6c97fec83396244c4e94d21720f07
이강산 <mountain@igangsan-ui-MacBook-Pro.local>
2018-03-28 19:59:16 +0900

    homework-02

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,10 +2,28 @@ var express = require('express');
 var router = express.Router();
 var ContactsModel = require('../models/ContactsModel');
 
+
+// contacts  글리스트
 router.get('/', function(req, res){
-    res.render('contacts/list');
+    ContactsModel.find(function(err, contacts){
+        res.render('contacts/list',{contacts:contacts});
+    });
 });
 
+// contacts/write 글작성
+router.get('/write', function(req, res){
+    res.render('contacts/form');
+});
+router.post('/write', function(req, res){
+    var contacts = new ContactsModel({
+        title:req.body.title, 
+        email:req.body.email, 
+        description:req.body.description
+    });
+    contacts.save(function(err){
+        res.redirect('/contacts')
+    });
+});
 /*
 URL
 /contacts  글리스트
