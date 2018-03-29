1cee7755e9d2d58b60f2c9f19dad3f1ab1e5306a
이강산 <mountain@igangsan-ui-MacBook-Pro-2.local>
2018-03-29 23:42:52 +0900

    contact CRUD 구현

--- routes/contacts.js
+++ routes/contacts.js
@@ -12,8 +12,9 @@ router.get('/', function(req, res){
 
 // contacts/write 글작성
 router.get('/write', function(req, res){
-    res.render('contacts/form');
+    res.render('contacts/form',{ contact : "" });
 });
+
 router.post('/write', function(req, res){
     var contacts = new ContactsModel({
         title:req.body.title, 
@@ -24,13 +25,50 @@ router.post('/write', function(req, res){
         res.redirect('/contacts')
     });
 });
+
+//contacts/detail/:id 상세글보기
+router.get('/detail/:id', function(req, res){
+    ContactsModel.findOne({'id':req.params.id},function(err, result){
+        if(err){
+            console.log(err);
+        }
+        res.render('contacts/detail', {contact : result});
+    });
+});
+
+//contacts/edit/:id 글수정하기
+router.get('/edit/:id', function(req, res){
+    ContactsModel.findOne({'id':req.params.id},function(err, result){
+        if(err){
+            console.log(err);
+        }
+        res.render('contacts/form', {contact : result});
+    });
+});
+
+router.post('/edit/:id', function(req, res){
+    var query = {
+        title:req.body.title, 
+        email:req.body.email, 
+        description:req.body.description
+    }
+    ContactsModel.update({ id : req.params.id}, { $set : query },function(err){
+        res.redirect('/contacts')
+    });
+});
+
+//contacts/delete/:id 글삭제하기
+router.get('/delete/:id', function(req, res){
+    ContactsModel.remove({ id : req.params.id}, function(err){
+        if(err){
+            console.log(err);
+        }
+        res.redirect('/contacts');
+    });
+});
 /*
 URL
-/contacts  글리스트
-/contacts/write 글작성
-/contacts/detail/:id  상세글보기
-/contacts/edit/:id 글수정하기
-/contacts/delete/:id 글삭제하기
+
 */
 
 module.exports = router;
