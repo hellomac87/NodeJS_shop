099cb1888f4a387a190346cddf623bd0d6e94715
이강산 <mountain@igangsan-ui-MacBook-Pro-2.local>
2018-04-02 20:24:06 +0900

    회원가입

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var UserModel = require('../models/UserModel');
+var passwordHash = require('../libs/passwordHash');
 
 router.get('/', function(req, res){
     res.render('accounts/login');
@@ -8,9 +9,23 @@ router.get('/', function(req, res){
 router.get('/login', function(req, res){
     res.render('accounts/login');
 })
+
+//회원가입
 router.get('/join', function(req, res){
     res.render('accounts/join');
 })
+router.post('/join', function(req, res){
+    var User = new UserModel({
+        username: req.body.username,
+        password:passwordHash(req.body.password),
+        displayname : req.body.displayname
+    });
+
+    User.save(function(err){
+        res.send('<script>alert("회원가입 성공");\
+        location.href="/accounts/login";</script>')
+    })
+});
 
 
 module.exports = router;
