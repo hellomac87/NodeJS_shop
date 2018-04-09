5d426cb566dee2f721a303a7e7d165290f736937
이강산 <mountain@igangsan-ui-MacBook-Pro-2.local>
2018-04-09 20:20:00 +0900

    2018-04-09 회원간 채팅 로그인 분기처리

--- routes/chat.js
+++ routes/chat.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
  
 router.get('/', function(req,res){
-    if(!req.isAuthenticated()){
+    if(!req.isAuthenticated()){ //passport에서 제공하는 로그인 체크 함수
         res.send('<script>alert("로그인이 필요한 서비스입니다.");\
         location.href="/accounts/login";</script>')
     }else{
